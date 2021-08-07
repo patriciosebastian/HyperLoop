4fc2faf43555353668cf474432191ca00c6e87ad
patriciosebastian <psebastiansalazar@gmail.com>
2021-08-07 00:35:52 -0700

    added branding for HyperLoop built out the rest of the UI and wrote the javascript

--- js/site.js
+++ js/site.js
@@ -1,4 +1,52 @@
-// Call Hello World
-function HelloWorld() {
-    alert('Hello World');
+// get the values from the page
+function getValues() {
+    // get values from the page input
+    let startValue = document.querySelector('#startValue').value;
+    let endValue =  document.querySelector('#endValue').value;
+
+    // always validate the input
+    // parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        // call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        // call displayNumbers
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter integers");
+    }
+}
+
+// generate numbers from startValue to the endValue
+// logic function(s)
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    // get all numbers from start to end
+    for (let i = startValue; i <= endValue; i++) {
+        // this will execute in a loop until index = endValue
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+// display the numbers and mark the even numbers in bold
+// display or view functions
+function displayNumbers(numbers) {
+    let templateRows = "";
+
+    for (let i = 0; i < numbers.length; i++) {
+        let className = "even";
+        let number = numbers[i];
+
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+    document.querySelector('#results').innerHTML = templateRows;
 }
